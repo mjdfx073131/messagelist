d73748631e865547ae8c0f828da288936f9bc9da
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-06-29 10:01:00 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import MessagesList from "./components/messages-list.component";
+import EditMessage from "./components/edit-message.component";
+import CreateMessage from "./components/create-message.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={MessagesList} />
+      <Route path="/edit/:id" component={EditMessage} />
+      <Route path="/create" component={CreateMessage} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
