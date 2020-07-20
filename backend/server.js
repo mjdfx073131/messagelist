1f6f47a6966a30b75b563b8c4741bdfecf5430d0
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-07-19 21:22:05 -0700

    13 deploy test

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '/../build')))
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-cchdc.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }
@@ -26,7 +26,7 @@ app.use('/users', usersRouter);
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../build/index.html'))
+  res.sendFile(path.join(__dirname + '/../build/index.html'))
 })
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
