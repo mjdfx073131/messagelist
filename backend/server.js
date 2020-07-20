7ded7643e84c08cc6e4ae1b03f67de1400abde42
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-07-19 19:21:01 -0700

    sfixth eploy test

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const path =require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../build')))
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-cchdc.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }
@@ -23,6 +24,10 @@ const usersRouter = require('./routes/users');
 app.use('/messages', messagesRouter);
 app.use('/users', usersRouter);
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../build/index.html'))
+})
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
