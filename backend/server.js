b6f1c3efba6e043e7c6e2eb7dbe28ae285768ae5
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-07-19 18:58:23 -0700

    second deploy test

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://m001-student:m001-mongodb-basics@sandbox-cchdc.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-cchdc.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
