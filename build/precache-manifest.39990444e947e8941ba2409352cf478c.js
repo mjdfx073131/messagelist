94695354852639511873bade3c4e53d4902cbbc1
kerry <q2k2b@ugrad.cs.ubc.ca>
2020-07-19 21:05:08 -0700

    12 deploy test

---
+++ build/precache-manifest.39990444e947e8941ba2409352cf478c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b69c97dbc37aa7f17bee9dc02a489de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b47a42245875c287b7",
+    "url": "/static/css/2.1601c95f.chunk.css"
+  },
+  {
+    "revision": "2ddd492f5ac52ff1e92d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0b47a42245875c287b7",
+    "url": "/static/js/2.45244c51.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.45244c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddd492f5ac52ff1e92d",
+    "url": "/static/js/main.712b70e1.chunk.js"
+  },
+  {
+    "revision": "22ab8c49c7c41dfeb75d",
+    "url": "/static/js/runtime-main.9915a679.js"
+  }
+]);
